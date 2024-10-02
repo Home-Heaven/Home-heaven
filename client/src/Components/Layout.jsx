a941c62696b847bad8e5981ffe28ab2e0c179141
punitsahu9039 <sahupunit1000@gmail.com>
2024-10-02 13:06:58 +0530

    component added

---
+++ client/src/Components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import out
+
+const Layout = () => {
+  return (
+    <>
+      <div className="mx-auto flex min-h-screen max-w-screen-xl flex-col">
+      </div>
+    </>
+  );
+};
+
+export default Layout;
